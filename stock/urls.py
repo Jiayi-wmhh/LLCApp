dd2c97ee474afb3f3e26e72575999c64c1e2a5d3
Jiayi <du.587@buckeyemail.osu.edu>
2022-03-30 20:55:34 -0500

    pre

--- stock/urls.py
+++ stock/urls.py
@@ -2,9 +2,6 @@ from django.urls import  path
 from . import views
 
 urlpatterns = [
-    path('',views.mainPage, name = "mainPage"),
-    path('stockInfo', views.stockInfo, name = "stockInfo" ),
-    path('sentimentAnalysis', views.sentimentAnalysis, name = "sentimentAnalysis" ),
-    path('SAresult', views.showSAResult, name = "showSAResult" ),
-    path('INFOresult', views.showInfoResult, name = "showInfoResult" ),
+    path('search', views.search, name = "search" ),
+    path('result', views.result, name = "result" ),
 ]
