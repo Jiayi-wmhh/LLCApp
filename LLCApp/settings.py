68c94a931502477ca32ec39444e6817ffbfb8c2f
Jiayi <du.587@buckeyemail.osu.edu>
2022-04-02 23:17:09 -0500

    pre

--- LLCApp/settings.py
+++ LLCApp/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, './LLCApp/LLCApp/static/')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'LLCApp/LLCApp/static/')
+    os.path.join(BASE_DIR, './LLCApp/LLCApp/static/')
 ]
 
 # Default primary key field type
