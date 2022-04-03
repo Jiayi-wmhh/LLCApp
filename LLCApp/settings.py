9ad85bd7e84194d66f0d5402a06e44cb59508360
Jiayi <du.587@buckeyemail.osu.edu>
2022-04-02 23:45:33 -0500

    pre

--- LLCApp/settings.py
+++ LLCApp/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+STATIC_URL = '/home/Jiayi/LLCApp/LLCApp/static'
 # STATIC_ROOT = os.path.join(BASE_DIR, './LLCApp/LLCApp/static/')
 STATIC_ROOT = "/home/Jiayi/LLCApp/LLCApp/static"
 # STATICFILES_DIRS = [
