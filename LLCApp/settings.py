7fd918005b77c0fbd3cf32dac682cdab73bd5502
Jiayi <du.587@buckeyemail.osu.edu>
2022-04-05 02:35:43 -0500

    effect

--- LLCApp/settings.py
+++ LLCApp/settings.py
@@ -120,12 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = '/static/'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, './LLCApp/LLCApp/static/')
+# STATIC_ROOT = os.path.join(BASE_DIR, './LLCApp/LLCApp/static/')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, './LLCApp/LLCApp/static/')
+# ]
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, './LLCApp/LLCApp/static/')
+    BASE_DIR.joinpath('static')
 ]
+STATIC_ROOT = '/home/Jiayi/LLCApp/LLCApp/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
