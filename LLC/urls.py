ea09a0e6d66e9c8c05ef901e1a7a7f4ed915dcae
Jiayi <du.587@buckeyemail.osu.edu>
2022-05-16 00:25:15 -0500

    change

--- LLC/urls.py
+++ LLC/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.homepage, name = 'homepage'),
+	path('main', views.homepage, name = 'homepage'),
     path('window', views.window, name='window'),
     path('graph', views.graph, name='graph'),
 ]
