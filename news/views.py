f11fb8ab89522bf3716560e55f23259f443d58c0
Jiayi <du.587@buckeyemail.osu.edu>
2022-08-04 13:03:35 -0700

    stack path

--- news/views.py
+++ news/views.py
@@ -16,15 +16,14 @@ import json
 import matplotlib.pyplot as plt
 import plotly.express as px
 from plotly.offline import plot
+from datetime import date
+from datetime import timedelta
 
 def search(request):
     return render(request,"search.html")
 
 def result(request):
 	ticker = request.POST.get('ticker')
-	url_stats = "https://finance.yahoo.com/quote/{}/key-statistics?p={}"
-	url_profile = "https://finance.yahoo.com/quote/{}/profile?p={}"
-	url_financials = "https://finance.yahoo.com/quote/{}/financials?p={}"
 	url_news = "https://finance.yahoo.com/quote/{}?p={}"
 	response = requests.get(url_news.format(ticker, ticker))
 	soup = BeautifulSoup(response.text)
@@ -33,35 +32,32 @@ def result(request):
 	terminal["statement"] = []
 	count = 0
 	price = soup.find('fin-streamer', {'class': 'Fw(b) Fz(36px) Mb(-4px) D(ib)'}).text
-	change = soup.find('fin-streamer', {'class': 'C($primaryColor) Fz(24px) Fw(b)'}).text
-	# for i,item in enumerate(change):
-	# 	if i == 10:
-	# 		change_amo = item.text
-	# 	if i == 11:
-	# 		change_per = item.text
+	# change = soup.find('fin-streamer', {'class': 'C($primaryColor) Fz(24px) Fw(b)'}).text
 	terminal["price"] = price
-	terminal["change"] = change
+	# terminal["change"] = change
 	terminal["hyper_spec"] = []
-	# terminal["change_per"] = change_per
-	news = soup.find_all('li', class_="js-stream-content Pos(r)")
-	for i in news:
-		title = i.find('h3', class_="Mb(5px)")
-		Statement = title.a.text
-		hyper = title.a.attrs["href"]
-		hyper = "https://finance.yahoo.com/"+hyper
-		terminal["statement"].append(Statement)
-		terminal["hyper_spec"].append(hyper)
-	pieName = ["Negative", "Neutral", "Positive"]
-	pieValue = [0, 0, 0]
 	terminal["nnp"] = []
 	peek = 0
+	terminal["table"] = []
+	yesterday = date.today()-timedelta(days = 1)
+	url_news = "https://finviz.com/quote.ashx?t="+ticker
+	req = Request(url=url_news,headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'}) 
+	response = urlopen(req)   
+	html = BeautifulSoup(response)
+	news_table = html.find(id='news-table')
+	pieName = ["Negative", "Neutral", "Positive"]
+	pieValue = [0, 0, 0]
+	for x in news_table.findAll('tr'):
+		Statement = x.a.get_text()
+		terminal["statement"].append(Statement)
+		hyper = x.a.attrs["href"]
+		terminal["hyper_spec"].append(hyper)
 	for i in terminal["statement"]:
 		sid_obj = SentimentIntensityAnalyzer()
 		sentiment_dict = sid_obj.polarity_scores(i)
 		pieValue[0] += sentiment_dict["neg"]
 		pieValue[1] += sentiment_dict["neu"]
 		pieValue[2] += sentiment_dict["pos"]
-		pieNameSmall = ["Negative", "Neutral", "Positive"]
 		pieValueSmall = [0, 0, 0, 0, "", ""]
 		pieValueSmall[0] = sentiment_dict["neg"]
 		pieValueSmall[1] = sentiment_dict["neu"]
@@ -78,57 +74,49 @@ def result(request):
 	# fig.update_layout(showlegend=False)
 	plot(fig, validate=False, filename='./LLCApp/templates/newsPie.html', 
 		auto_open=False)
-	smallPie = list(range(1, count+1))
-	for i in range(1, count+1):
-		smallPie[i-1] = "newsPie"+str(i)+".html"
-	terminal["smallPie"] = smallPie
-	
 
-	url_business = "https://finance.yahoo.com/news/"
-	response = requests.get(url_business)
-	soup = BeautifulSoup(response.text)
-	terminal["business"] = []
-	bus_news = soup.find_all('h3', class_="Mb(5px)")
-	for i in bus_news:
-		bus_title = i.text
-		terminal["business"].append(bus_title)
-	terminal["nnp_bus"] = []
-	pieValueBusiness = [0, 0, 0]
-	for i in terminal["business"]:
-		sid_obj = SentimentIntensityAnalyzer()
-		sentiment_dict = sid_obj.polarity_scores(i)
-		pieValueBusiness[0] += sentiment_dict["neg"]
-		pieValueBusiness[1] += sentiment_dict["neu"]
-		pieValueBusiness[2] += sentiment_dict["pos"]
-		bus_value = [0, 0, 0, 0,""]
-		bus_value[0] = sentiment_dict["neg"]
-		bus_value[1] = sentiment_dict["neu"]
-		bus_value[2] = sentiment_dict["pos"]
-		bus_value[3] = sentiment_dict["compound"]
-		bus_value[4] = i
-		terminal["nnp_bus"].append(bus_value)
-	figBus = px.pie(values=pieValueBusiness, names=pieName,color=pieName,color_discrete_map={'Negative': 'red',
+
+	terminal["ticker_list"] = ['AAPL', 'MSFT', 'NVDA', 'V', 'MA', 'AVGO',
+	'ADBE', 'CSCO', 'ACN', 'CRM', 'QCOM', 'INTC', 'TXN', 'AMD', 'INTU', 'ORCL', 
+	'IBM', 'PYPL', 'ADP', 'AMAT', 'NOW', 'ADI', 'MU', 'LRCX', 'FIS', 'FISV',
+	'KLAC', 'SNPS', 'CDNS', 'NXPI', 'ROP', 'ADSK', 'APH', 'FTNT', 'TEL', 'PAYX',
+	'MSI', 'MCHP', 'CTSH', 'HPQ', 'GPN', 'ENPH', 'KEYS','GLW','ON','ANET','CDW',
+	'ANSS','IT','MPWR','TDY','EPAM', 'BR', 'HPE', 'VRSN', 'ZBRA', 'SWKS', 'FLT',
+	'SEDG', 'TRMB', 'TER', 'NTAP', 'PAYC', 'TYL', 'STX', 'AKAM', 'WDC', 'NLOK',
+	'JKHY', 'CTXS', 'PTC', 'QRVO', 'FFIV', 'JNPR', 'DXC', 'CDAY']
+	terminal["tt_res"] = []
+	for i in terminal["ticker_list"]:
+		url_tt = "https://finviz.com/quote.ashx?t="+i
+		req = Request(url=url_tt,headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'}) 
+		response = urlopen(req)   
+		soup = BeautifulSoup(response)
+		news_tt = soup.find(id='news-table')
+		tt_curr = [0.0, 0.0, 0.0, 0.0, i]
+		for x in news_tt.findAll('tr'):
+			Statement = x.a.get_text()
+			sid_obj = SentimentIntensityAnalyzer()
+			sentiment_dict = sid_obj.polarity_scores(Statement)
+			tt_curr[0] += sentiment_dict["neg"]
+			tt_curr[1] += sentiment_dict["neu"]
+			tt_curr[2] += sentiment_dict["pos"]
+			tt_curr[3] += sentiment_dict["compound"]
+		tt_curr[0] = round(tt_curr[0], 3)
+		tt_curr[1] = round(tt_curr[1], 3)
+		tt_curr[2] = round(tt_curr[2], 3)
+		tt_curr[3] = round(tt_curr[3], 3)
+		terminal["tt_res"].append(tt_curr)
+	tt_value = [0, 0, 0]
+	for i in terminal["tt_res"]:
+		tt_value[0] += i[0]
+		tt_value[1] += i[1]
+		tt_value[2] += i[2]
+	fig = px.pie(values=tt_value, names=pieName, color=pieName,color_discrete_map={'Negative': 'red',
 		'Neutral': 'yellow', 'Positive': 'green'})
-	figBus.update_traces(textposition='inside')
-	figBus.update_layout(margin=dict(t=0, b=0, l=0, r=0), showlegend=False,)
+	fig.update_traces(textposition='inside')
+	fig.update_layout(margin=dict(t=0, b=0, l=0, r=0), showlegend=False,)
 	# fig.update_layout(showlegend=False)
-	plot(figBus, validate=False, filename='./LLCApp/templates/newsPieBus.html', 
+	plot(fig, validate=False, filename='./LLCApp/templates/ttPie.html', 
 		auto_open=False)
 
-
-	line_neg = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-	line_neu = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-	line_pos = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-	line_date = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-	for i in range(10):
-		line_neg[i] = terminal["nnp"][i][0] + terminal["nnp_bus"][i][0]
-		line_neu[i] = terminal["nnp"][i][1] + terminal["nnp_bus"][i][1]
-		line_pos[i] = terminal["nnp"][i][2] + terminal["nnp_bus"][i][2]
-	plt.plot(line_date, line_neg, 'r', label="Negative")
-	plt.plot(line_date, line_neu, 'y', label="Neural")
-	plt.plot(line_date, line_pos, 'g', label="Positive")
-	plt.xlabel('Days')
-	plt.ylabel('Analyze Range')
-	plt.savefig("./LLCApp/LLCApp/static/line_graph")
 
 	return render(request,"result.html", {'terminal':terminal.items()}) 
